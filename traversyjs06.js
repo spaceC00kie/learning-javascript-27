2ccf478076743b767ad2ca50d8978a9f0986b3ea
spaceC00kie <kirstie317@gmail.com>
2020-12-09 22:40:27 -0500

    arrays

---
+++ traversyjs06.js
@@ -0,0 +1,22 @@
+//const s = "technology, computers, it, code"
+
+//console.log(s.split(", "))
+
+//Arrays - variable that hold multiple values
+
+// array constructor - less used
+//const numbers = new Array(1, 2, 3, 4, 5)
+//console.log(numbers)
+
+const fruits = ["apples", "oranges", "pears", "grapes", 42, true]
+
+fruits[6] = "cherries"
+
+fruits.push("mangos")
+
+fruits.unshift("strawberries")
+
+console.log(fruits)
+//console.log(fruits[1]) - returns second item in array
+
+//JS IS NOT STATICALLY TYPED
